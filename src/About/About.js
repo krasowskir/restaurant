84e02293e1fb5810fd3901aec29548ed9021f26e
Richard Krasowski <r.krasowski@tarent.de>
2018-12-12 17:40:07 +0100

    version 4.4 -About Block hinzugefügt -scss angepasst, damit bei small viewport der Text in voller Breite erscheint und bei medium nur in halber

---
+++ src/About/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./about.scss";
+
+const About = props => (
+  <div className="about row">
+    <h2>About</h2>
+    <div className="row">
+      <div className="col col-sm-12 col-md-6 ">
+        <h4>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+          diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+          dolor sit amet. Lorem ipsum dolor si
+        </h4>
+      </div>
+    </div>
+  </div>
+);
+
+export default About;
--- src/MeinContainer/MeinContainer.js
+++ src/MeinContainer/MeinContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import sky from "../../public/assets/sky.jpg";
+import About from "../About/About";
 import "./container.scss";
 
 export default class MeinComponent extends Component {
@@ -11,6 +12,7 @@ export default class MeinComponent extends Component {
     return (
       <div className="container">
         <img src={sky} alt="..." className="meinImage" />
+        <About />
       </div>
     );
   }
