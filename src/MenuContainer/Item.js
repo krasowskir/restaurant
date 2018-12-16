63e6b435311a6796a5710345bd69ce113cf2023e
Richard Krasowski <r.krasowski@tarent.de>
2018-12-16 20:31:56 +0100

    version 6.4 -ItemRow refactored, kann jetzt Wrapper und Kindkomponenten in einem rendern

--- src/MenuContainer/Item.js
+++ src/MenuContainer/Item.js
@@ -1,33 +1,34 @@
 import React from "react";
 
 const ItemRow = props => {
-  const rows = props.group.items % 3;
-  const firstRow = props.groupen.items.slice(0, 3);
-  const secondRow = props.group.items.slice(3);
-  let i = 0;
+  const rowsCount = Math.floor(props.group.items.length / 3);
+  console.log("rowscount " + rowsCount);
+  const rows = [];
+
+  for (let i = 0; i < rowsCount; i++) {
+    let currentrow = props.group.items.slice(i * 3, i * 3 + 3);
+    console.log("currentrow " + currentrow);
+    rows.push(currentrow);
+  }
+  console.log(JSON.stringify(rows));
 
   return (
     <div className="row">
       <div className="col-12">
         <h2 className="page-header">{props.group.name}</h2>
-        <div className="row">
-          {firstRow.map(item => (
-            <div className="col-3 offset-1  menu__cell" id={i++}>
-              <h3>{item.name}</h3>
-              <p>{item.description}</p>
-              <p className="price">{item.price}</p>
+        {rows.map(row => {
+          return (
+            <div className="row">
+              {row.map(item => (
+                <div className="col-3 offset-1  menu__cell">
+                  <h3>{item.name}</h3>
+                  <p>{item.description}</p>
+                  <p className="price">{item.price}</p>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="row">
-          {secondRow.map(item => (
-            <div className="col-3 offset-1  menu__cell" id={i++}>
-              <h3>{item.name}</h3>
-              <p>{item.description}</p>
-              <p className="price">{item.price}</p>
-            </div>
-          ))}
-        </div>
+          );
+        })}
       </div>
     </div>
   );
